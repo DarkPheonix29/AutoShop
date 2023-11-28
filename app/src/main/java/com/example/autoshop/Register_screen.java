5f3f55de7e8f1ab05186791a23c42ffb3cb60885
Kealan Thal <kealanthal29@gmail.com>
2023-11-28 11:36:20 +0100

    register update 3

--- app/src/main/java/com/example/autoshop/Register_screen.java
+++ app/src/main/java/com/example/autoshop/Register_screen.java
@@ -1,15 +1,33 @@
 package com.example.autoshop;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class Register_screen extends AppCompatActivity {
+
+    private CheckBox agreeCheckBox;
+    private TextView agreeMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_screen);
+
+        agreeCheckBox = findViewById(R.id.checkBox);
+        agreeMessage = findViewById(R.id.agree_message);
+    }
+
+    public void switchToMainActivity(View view) {
+        if (agreeCheckBox.isChecked()) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            // Show the message
+            agreeMessage.setVisibility(View.VISIBLE);
+        }
     }
 }
