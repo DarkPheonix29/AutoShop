31be728cebc42a364cf2ad7fc129749a6d15cc5b
Kealan Thal <kealanthal29@gmail.com>
2023-11-28 10:55:09 +0100

    Initial commit

---
+++ app/src/main/java/com/example/autoshop/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.autoshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void switchToSecondActivity(View view) {
+        Intent intent = new Intent(this, Register_screen.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/autoshop/Register_screen.java
@@ -0,0 +1,15 @@
+package com.example.autoshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+public class Register_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_screen);
+    }
+}
