a97d19274859ba7e1643a778a8b3fed0f8458dc8
Kealan Thal <kealanthal29@gmail.com>
2023-12-08 09:16:58 +0100

    Correct button paths

--- app/src/main/java/com/example/autoshop/MainActivity.java
+++ app/src/main/java/com/example/autoshop/MainActivity.java
@@ -5,40 +5,16 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // You can also set click listeners programmatically if needed
-        Button loginButton = findViewById(R.id.log_in);
-        Button registerButton = findViewById(R.id.Register);
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switchToSecondActivity();
-            }
-        });
-
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switchToRegisterScreen();
-            }
-        });
     }
 
-    private void switchToSecondActivity() {
+    public void switchToSecondActivity(View view) {
         Intent intent = new Intent(this, ProductScreen.class);
-        startActivity(intent);
-    }
-
-    private void switchToRegisterScreen() {
-        Intent intent = new Intent(this, Register_screen.class);
         startActivity(intent);
     }
 }
