b18c66a794b2063251db3c04ed18c8dbfc7012f1
Kealan Thal <kealanthal29@gmail.com>
2023-12-08 12:31:14 +0100

    Button changed, CORRECT

--- app/src/main/java/com/example/autoshop/MainActivity.java
+++ app/src/main/java/com/example/autoshop/MainActivity.java
@@ -11,10 +11,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Assuming you have buttons with IDs button1 and button2
+        Button button1 = findViewById(R.id.log_in);
+        Button button2 = findViewById(R.id.Register);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchToFirstActivity();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchToSecondActivity();
+            }
+        });
     }
 
-    public void switchToSecondActivity(View view) {
-        Intent intent = new Intent(this, ProductScreen.class);
+    private void switchToFirstActivity() {
+        Intent intent = new Intent(this, ProductScreen.class); // Replace FirstActivity.class with the actual class for the first activity
+        startActivity(intent);
+    }
+
+    private void switchToSecondActivity() {
+        Intent intent = new Intent(this, Register_screen.class); // Replace SecondActivity.class with the actual class for the second activity
         startActivity(intent);
     }
 }
